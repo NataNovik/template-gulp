01a2900a6cce65a89fd7d5ad18109db46adea226
Novik <natalyamihalovna88@gmail.com>
2022-05-02 14:06:55 +0300

    feat(gulp): added comment to function

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const {dest, src, parallel} = require('gulp');
 const imagemin = require('gulp-imagemin');
-
+// /--image compression function--
 function images() {
     return src('./src/assets/images/**/*')
         .pipe(imagemin())
